143e0d4fafc2ab44bbc3fe97520327bbee2fc62f
cheraa-git <chernetsov.aa@mail.ru>
2023-09-10 23:12:52 +0400

    bug fix

--- src/router/mainRouter.ts
+++ src/router/mainRouter.ts
@@ -2,6 +2,7 @@ import { Express } from 'express'
 import { reportRouter } from './routes/report.route'
 import { templateRouter } from './routes/template.route'
 import { customerOrderRouter } from './routes/customerOrder.route'
+import { fileRouter } from './routes/file.route'
 
 export class MainRouter {
   constructor(private app: Express) {
@@ -11,6 +12,6 @@ export class MainRouter {
     this.app.use('/reports', reportRouter)
     this.app.use('/template', templateRouter)
     this.app.use('/customer_order', customerOrderRouter)
-    this.app.use('/file')
+    this.app.use('/file', fileRouter)
   }
 }
