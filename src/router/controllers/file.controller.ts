5847995120a34b8d26288ab9866b14247cbc788d
cheraa-git <chernetsov.aa@mail.ru>
2023-09-11 00:23:44 +0400

    multer

--- src/router/controllers/file.controller.ts
+++ src/router/controllers/file.controller.ts
@@ -3,7 +3,8 @@ import { storageService } from '../../services/storage.service'
 
 export class FileController {
   saveFile = async (req: Request, res: Response) => {
-    const { file } = req.body
+    // const { file } = req.body
+    const file = req.file?.buffer
     const storageType = 'firebase'
     const dirName = 'test_dir'
     const fileName = 'testF_file_name'
@@ -12,8 +13,7 @@ export class FileController {
       return res.status(400).send('file is undefined')
     }
    if (storageType === 'firebase') {
-     const bufferFile = Buffer.from(file)
-     const fileUrl = await storageService.save(bufferFile, dirName, fileName)
+     const fileUrl = await storageService.save(file, dirName, fileName)
       res.send({url: fileUrl})
     } else {
       return res.status(400).send({ message: `invalid storage type - ${storageType}` })
--- src/router/routes/file.route.ts
+++ src/router/routes/file.route.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import { FileController } from '../controllers/file.controller'
+import multer from 'multer'
 
 const controller = new FileController()
+const upload = multer()
+
 export const fileRouter = Router()
 
-fileRouter.post('/', controller.saveFile)
+fileRouter.post('/', upload.single('file'), controller.saveFile)
 
 
