c5320245deef342999a6edceae8452c219c05757
cheraa-git <chernetsov.aa@mail.ru>
2023-09-10 23:21:27 +0400

    .

--- src/router/controllers/file.controller.ts
+++ src/router/controllers/file.controller.ts
@@ -10,7 +10,7 @@ export class FileController {
       const fileUrl = await storageService.save(file, dirName, fileName)
       res.send({url: fileUrl})
     } else {
-      return res.status(400).send({ message: 'invalid storage type' })
+      return res.status(400).send({ message: `invalid storage type - ${storageType}` })
     }
   }
 }
