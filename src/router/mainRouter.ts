99f06d165c0f324cf17867408b5692de033930e7
cheraa-git <chernetsov.aa@mail.ru>
2023-09-15 18:55:27 +0400

    remove file router

--- src/router/mainRouter.ts
+++ src/router/mainRouter.ts
@@ -2,7 +2,6 @@ import { Express } from 'express'
 import { reportRouter } from './routes/report.route'
 import { templateRouter } from './routes/template.route'
 import { customerOrderRouter } from './routes/customerOrder.route'
-import { fileRouter } from './routes/file.route'
 
 export class MainRouter {
   constructor(private app: Express) {
@@ -12,6 +11,5 @@ export class MainRouter {
     this.app.use('/reports', reportRouter)
     this.app.use('/template', templateRouter)
     this.app.use('/customer_order', customerOrderRouter)
-    this.app.use('/file', fileRouter)
   }
 }
